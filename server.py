bf88aed9938f3285bc3f4e3075ff4d79518ffdde
sirinbuyukkeskin-beep <sirinbuyukkeskin@gmail.com>
2026-01-09 20:15:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "FashionAI Backend Çalışıyor!"
+    return "FashionAI Beçkent çalışıyor!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=140)
